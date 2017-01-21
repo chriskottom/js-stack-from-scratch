71e3cca3852b323ac761119155c04c8c0776a7a1
chriskottom <chris@chriskottom.com>
2017-01-21 18:42:00 +0100

    Add React and turn the App into a React app

---
+++ src/client/app.jsx
@@ -0,0 +1,19 @@
+import 'babel-polyfill';
+
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import Dog from '../shared/dog';
+
+const dogBark = new Dog('Browser Toby').bark();
+
+const App = props => (
+  <div>
+    The doc says: {props.message}
+  </div>
+);
+
+App.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
+ReactDOM.render(<App message={dogBark} />, document.querySelector('.app'));
